fd2bb7ab81c4795699af9d410bbb934a8ccf5848
Harsih6777 <85174806+Harsih6777@users.noreply.github.com>
2021-06-02 00:33:47 -0700

    9th file

---
+++ Ropp.java
@@ -0,0 +1,26 @@
+class Ropp{
+
+public static voiud main(String[] args){
+
+int a=7;
+ 
+int b=6;
+
+System.oput.println("a is "+ a + " and " b is " + b);
+
+
+System.out.println(a==b);
+
+
+System.out.println(a!=b);
+
+System.out.println(a>b);
+
+System.out.println(a<b);
+
+System.out.println(a>=b);
+
+System.out.println(a<=b);
+
+}
+}
