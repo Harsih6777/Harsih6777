fa983c099b37132678f9eadb45c58b7b58043802
Harsih6777 <85174806+Harsih6777@users.noreply.github.com>
2021-06-02 00:25:56 -0700

    2nd file

---
+++ UsingArray.java
@@ -0,0 +1,18 @@
+    class UsingArray{
+
+   public static void main(String[] args){
+
+
+int[] age={1,32,45};
+
+System.out.println("Using loop");
+
+for(int i=0; i < age.length ; i++){
+
+System.out.println(age[i]);
+
+}
+}
+}
+
+  
