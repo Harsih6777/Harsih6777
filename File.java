94ca001b95db58a037528e0005823a7b8a66ea90
Harsih6777 <85174806+Harsih6777@users.noreply.github.com>
2021-06-02 00:32:16 -0700

    8th file

---
+++ File.java
@@ -0,0 +1,23 @@
+  class File{
+
+
+public static void main(String[] args){
+
+int a=26;
+
+int b=6;
+
+
+System.out.println("a+b="+(a+b));
+
+System.out.println("a-b="+(a-b));
+
+System.out.println("a*b="+(a*b));
+
+System.out.println("a/b="+(a/b));
+
+
+System.out.println("a%b="+(a%b));
+
+}
+}
