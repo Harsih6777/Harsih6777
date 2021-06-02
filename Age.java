df86f146d63d54fe5b81d6983546923c9e9ae4b3
Harsih6777 <85174806+Harsih6777@users.noreply.github.com>
2021-06-02 00:31:21 -0700

    7th file

---
+++ Age.java
@@ -0,0 +1,28 @@
+   
+
+   class Age{
+
+   public static void main(String[] args){
+
+
+  int[] age ={12,34,56,78,98};
+
+  System.out.println("Accenssing Element of Array");
+
+  System.out.println("First Element:" +age[0]);
+
+
+  System.out.println("First Element:" +age[1]);
+
+
+  System.out.println("Second Element:" +age[2]);
+
+
+  System.out.println("Third Element:" +age[3]);
+
+
+
+  System.out.println("Fourth Element:" +age[4]);
+
+   }
+}
