13fb28215fddaaba6c4cb28b9ba81bb63997a1b1
Harsih6777 <85174806+Harsih6777@users.noreply.github.com>
2021-06-06 21:57:18 -0700

    Add files via upload

---
+++ MutidimensionalArray1.java
@@ -0,0 +1,32 @@
+  
+
+
+ class MultidimenionalArray1{
+
+  
+ public static void main(String[] args){
+
+
+   int[][] a= {
+
+
+    {1,23,45},
+
+    {35,66,77,87,9},
+    
+    {98}
+};
+
+for(int i=0;i <a.length; i++){
+
+
+for(int j =0; j < a[i].length; j++){
+
+    System.out.println(a[i][j]);
+
+}
+
+
+}
+}
+}
