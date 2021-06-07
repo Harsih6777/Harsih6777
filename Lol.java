dbab425668b52768feb913503f3e5463da17e38c
Harsih6777 <85174806+Harsih6777@users.noreply.github.com>
2021-06-06 21:56:12 -0700

    logical opperators

---
+++ Lol.java
@@ -0,0 +1,25 @@
+class Lol{
+
+public static void main(String[] args){
+
+
+System.out.println((5 > 3) && (8 > 5));
+
+System.out.println((5 > 3) && (8 < 5));
+
+
+
+
+System.out.println((5 < 3) || (8 > 5));
+
+System.out.println((5 > 3) || (8 < 5));
+
+System.out.println((5 < 3) || (8 < 5));
+
+System.out.println(!(5 == 3));
+
+
+System.out.println(!(5 > 3));
+
+}
+}
