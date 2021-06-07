4aa9aa70df09636cef6cff96163ac758115ead9e
Harsih6777 <85174806+Harsih6777@users.noreply.github.com>
2021-06-06 21:48:25 -0700

    increment&decrement

---
+++ InDe.java
@@ -0,0 +1,25 @@
+class InDe{
+
+public static void main(String[] args){
+
+int a=12;
+int b =13;
+
+int result1;
+
+int result2;
+
+System.out.println("Value of a :"+a);
+
+result1 =++a;
+
+
+System.out.println("After Increment:"+result1);
+
+System.out.println("value of b :"+b);
+
+result2 =--b;
+
+System.out.println("After decrement:"+result2);
+}
+}
