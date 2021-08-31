df27fc6f7730bafbe7a55f7adb81eac8706914a8
Harsih6777 <85174806+Harsih6777@users.noreply.github.com>
2021-08-31 08:04:41 +0530

    Add files via upload

---
+++ LineMultiple.java
@@ -0,0 +1,12 @@
+public class LineMultiple{
+
+ public static void main(String[] args){
+
+ String[]  nameArray = {"Neeraj","Chopra","bagged","Gold","Metal", "in","javlin"};
+
+int len = nameArray.length;
+
+System.out.println("Length of nameArray{\"Neeraj\",\"Chopra\",\"bagged\",\"Gold\",\"Metal\" \"in\",\"javlin\"}:" + len);
+
+}
+}
