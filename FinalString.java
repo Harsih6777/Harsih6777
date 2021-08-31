5eb68c9556a2963cbb6dbe1386b797b96dc06320
Harsih6777 <85174806+Harsih6777@users.noreply.github.com>
2021-08-31 08:04:16 +0530

    Add files via upload

---
+++ FinalString.java
@@ -0,0 +1,16 @@
+public class FinalString{
+
+  public static void main(String[] args){
+
+StringBuilder s1 = new StringBuilder("");
+
+  System.out.println("String length is:"+s1.length());
+
+  StringBuilder s2 = new StringBuilder("java");
+
+   System.out.println("String length is:"+s2.length());
+
+    System.out.println("String is:"+s2.toString());
+
+}
+}
