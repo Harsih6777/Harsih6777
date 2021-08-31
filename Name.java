aeb94578e928116eba02e5ac04ba5659fde43ba5
Harsih6777 <85174806+Harsih6777@users.noreply.github.com>
2021-08-31 07:57:36 +0530

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+ class Name {
+
+  public static void main(String[] args){
+
+  HashMap<String,String> name = new HashMap<String,String>();
+    name.put("Harish","kumar");
+  
+    name.put("nathan","seetha");
+  
+   System.out.println(name);
+
+    System.out.println(name.size());
+}
+}
