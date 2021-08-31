ea3842c18c9de14e37a491b07a1e1ecf538011dd
Harsih6777 <85174806+Harsih6777@users.noreply.github.com>
2021-08-31 08:03:45 +0530

    Add files via upload

---
+++ EqualString.java
@@ -0,0 +1,19 @@
+public class EqualString {
+
+  public static void main(String[] args){
+
+
+  String str1 = "india";
+ 
+  String str2 = "india";
+
+  String str3 = "Einstein";
+
+  String str4 = "einstein";
+
+  System.out.println(str1 == str2 );
+ 
+ System.out.println(str3 == str4 );
+
+}
+}
