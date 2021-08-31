6f4d280acd0319f939455414b7c0f349f6cbc6b2
Harsih6777 <85174806+Harsih6777@users.noreply.github.com>
2021-08-31 08:05:35 +0530

    Add files via upload

---
+++ WithString.java
@@ -0,0 +1,29 @@
+public class WithString{
+
+  public static void main(String[] args){
+
+  StringBuilder s = new StringBuilder("Testing");
+ 
+ System.out.println("String length is:"+s.length());
+
+   StringBuilder s1  = new StringBuilder ("Team");
+  
+   StringBuilder s2 =  new StringBuilder("is");
+
+   StringBuilder s3  = new StringBuilder("my");
+
+   StringBuilder s4  = new StringBuilder("Team");
+   
+
+   System.out.println("String length is:"+s1.length());
+  
+    System.out.println("String length is:"+s2.length());
+
+    System.out.println("String length is:"+s3.length());
+
+
+     System.out.println("String length is:"+s4.length());
+
+ 
+}
+}
