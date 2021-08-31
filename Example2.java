0ba049398b2b642bdf7d9e5fa437a67c117373c9
Harsih6777 <85174806+Harsih6777@users.noreply.github.com>
2021-08-31 08:06:30 +0530

    Add files via upload

---
+++ Example2.java
@@ -0,0 +1,8 @@
+class Example2{
+public static void main(String[] args){
+String s1 = s1.parseInt(args[0]);
+String s2= s2.parseInt(args[1]);
+int String = s1 + s2;
+System.out.println("The string is" +String);
+}
+}
