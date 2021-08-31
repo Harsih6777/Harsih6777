ad1b5b9e5d2c196474055c5350bf61ae71aba3e2
Harsih6777 <85174806+Harsih6777@users.noreply.github.com>
2021-08-31 07:55:52 +0530

    Add files via upload

---
+++ Cars.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+ public class Cars {
+
+   public static void main(String[] args){
+
+      ArrayList<String> cars = new ArrayList<>();
+ 
+     cars.add("Thar");
+
+System.out.println(cars);
+ 
+   int size = cars.size(); 
+ System.out.println("ArrayList Size: " + size);
+
+
+
+}
+}
